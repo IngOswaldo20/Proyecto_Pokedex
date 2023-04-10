e1ca28f21c51cec556abd839a3b71fade1502a17
IngOswaldo20 <oswaldobarajas83@gmail.com>
2023-04-09 21:39:36 -0600

    Se agrega la eleccion de pokemones

--- js/lucha.js
+++ js/lucha.js
@@ -4,13 +4,40 @@ export function getPokemonInfo(pokemon) {
     return fetch(url)
       .then(response => response.json())
       .then(data => {
+        const pokeImg = document.querySelector('[data-poke-img]');
         const vida = data.stats[0].base_stat;
         const ataque = data.stats[1].base_stat;
         const nombre =  data.name;
+        console.log(vida);
+        const sprite =  data.sprites.front_default;
+        pokeImg.setAttribute('src', sprite);
+        pokeImg.style.backgroundSize = ' 5px 5px';
+        return { vida, ataque , nombre};
+      });
+  }
+
+  export function getPokemonInfo2(pokemon) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+  
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const pokeImg2 = document.querySelector('[data-poke-img2]');
+        const vida = data.stats[0].base_stat;
+        const ataque = data.stats[1].base_stat;
+        const nombre =  data.name;
+        const sprite =  data.sprites.front_default;
+        pokeImg2.setAttribute('src', sprite);
+        pokeImg2.style.backgroundSize = ' 5px 5px';
         return { vida, ataque , nombre};
       });
   }
   
+function pelear(nombre, vida, ataque){
+  
+}
+  
+
 var menuAudio = document.getElementById("menuAudio");
 menuAudio.volume = 0.3; 
 
